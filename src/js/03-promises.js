fe50490a046bf5225e857ab300423b4c9e3852f9
YuriiRodenko <jury.rodenko@gmail.com>
2023-05-03 18:08:21 +0300

    add task_3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  const { elements: { delay, step, amount } } = e.currentTarget;
+
+  for (
+    let i = 1, j = Number(delay.value);
+    i <= amount.value;
+    i += 1, j += Number(step.value)
+  ) {
+    createPromise(i, j);
   }
+  e.target.reset();
+}
+
+function createPromise(position, delay) {
+  return new Promise(() => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      } else {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      }
+    }, delay);
+  });
 }
