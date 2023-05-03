377fe80a76d0f1b286e2921bed976d11dde22e63
YuriiRodenko <jury.rodenko@gmail.com>
2023-05-03 16:47:18 +0300

    add task_1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+let id = null;
 
+btnStop.setAttribute('disabled', 'disabled');
+
+btnStart.addEventListener('click', onClickStart);
+btnStop.addEventListener('click', onClickStop);
+
+function onClickStart() {
+  btnStart.setAttribute('disabled', 'disabled');
+  btnStop.removeAttribute('disabled');
+  id = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onClickStop() {
+  clearInterval(id);
+  btnStart.removeAttribute('disabled');
+  btnStop.setAttribute('disabled', 'disabled');
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
